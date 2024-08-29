5fab2a587a4f02f2cf2a5785a74f701b68c2b277
HK <hkoh2@wgu.edu>
2024-08-28 22:44:09 -0400

    improved visuals

--- src/blackjack.py
+++ src/blackjack.py
@@ -4,10 +4,10 @@ from lib.player import Player
 from lib.dealer import Dealer
 
 new_dealer = Dealer
-player_1 = Player
-player_2 = Player
-player_3 = Player
-player_4 = Player
+player_1 = Player()
+player_2 = Player()
+player_3 = Player()
+player_4 = Player()
 players = [player_1, player_2, player_3, player_4]
 
 new_table = Table(new_dealer, players)
