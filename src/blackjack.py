ddb7e978b081c065f82b2b3afab225ab75964a88
HK <hkoh2@wgu.edu>
2024-09-02 17:10:46 -0400

    implement deal

--- src/blackjack.py
+++ src/blackjack.py
@@ -3,11 +3,11 @@ from lib.table import Table
 from lib.player import Player
 from lib.dealer import Dealer
 
-new_dealer = Dealer
-player_1 = Player()
-player_2 = Player()
-player_3 = Player()
-player_4 = Player()
+new_dealer = Player("Dealer")
+player_1 = Player("John")
+player_2 = Player("Dave")
+player_3 = Player("Matt")
+player_4 = Player("Sean")
 players = [player_1, player_2, player_3, player_4]
 
 new_table = Table(new_dealer, players)
